ed2f6fe2e8b82ceba2aece1599d88861f96ee6f3
Tue Vu <vu.tue.1710@gmail.com>
2018-02-04 17:48:40 +0700

    first commit on develop

--- app/containers/FeaturePage/messages.js
+++ app/containers/FeaturePage/messages.js
@@ -19,77 +19,16 @@ export default defineMessages({
     defaultMessage: `Automate the creation of components, containers, routes, selectors
   and sagas - and their tests - right from the CLI!`,
   },
-  feedbackHeader: {
-    id: 'boilerplate.containers.FeaturePage.feedback.header',
-    defaultMessage: 'Instant feedback',
-  },
-  feedbackMessage: {
-    id: 'boilerplate.containers.FeaturePage.feedback.message',
-    defaultMessage: `
-      Enjoy the best DX and code your app at the speed of thought! Your
-    saved changes to the CSS and JS are reflected instantaneously
-    without refreshing the page. Preserve application state even when
-    you update something in the underlying code!
-    `,
-  },
-  stateManagementHeader: {
-    id: 'boilerplate.containers.FeaturePage.state_management.header',
-    defaultMessage: 'Predictable state management',
-  },
-  stateManagementMessages: {
-    id: 'boilerplate.containers.FeaturePage.state_management.message',
-    defaultMessage: `
-      Unidirectional data flow allows for change logging and time travel
-    debugging.
-    `,
-  },
-  javascriptHeader: {
-    id: 'boilerplate.containers.FeaturePage.javascript.header',
-    defaultMessage: 'Next generation JavaScript',
-  },
-  javascriptMessage: {
-    id: 'boilerplate.containers.FeaturePage.javascript.message',
-    defaultMessage: `Use template strings, object destructuring, arrow functions, JSX
-    syntax and more, today.`,
-  },
-  cssHeader: {
-    id: 'boilerplate.containers.FeaturePage.css.header',
-    defaultMessage: 'Features',
-  },
-  cssMessage: {
-    id: 'boilerplate.containers.FeaturePage.css.message',
-    defaultMessage: 'Next generation CSS',
-  },
-  routingHeader: {
-    id: 'boilerplate.containers.FeaturePage.routing.header',
-    defaultMessage: 'Industry-standard routing',
-  },
-  routingMessage: {
-    id: 'boilerplate.containers.FeaturePage.routing.message',
-    defaultMessage: `
-      Write composable CSS that's co-located with your components for
-    complete modularity. Unique generated class names keep the
-    specificity low while eliminating style clashes. Ship only the
-    styles that are on the page for the best performance.
-    `,
-  },
-  networkHeader: {
-    id: 'boilerplate.containers.FeaturePage.network.header',
-    defaultMessage: 'Offline-first',
-  },
-  networkMessage: {
-    id: 'boilerplate.containers.FeaturePage.network.message',
-    defaultMessage: `
-      The next frontier in performant web apps: availability without a
-      network connection from the instant your users load the app.
-    `,
-  },
-  intlHeader: {
-    id: 'boilerplate.containers.FeaturePage.internationalization.header',
-    defaultMessage: 'Complete i18n Standard Internationalization & Pluralization',
-  },
-  intlMessage: {
-    id: 'boilerplate.containers.FeaturePage.internationalization.message',
-    defaultMessage: 'Scalable apps need to support multiple languages, easily add and support multiple languages with `react-intl`.',
-  },
+  items: [
+    {
+      name: 'TO DO APP',
+      description: 'Application to create todo list. Enable to add, remove, edit, filter, ... todo item',
+      to: '/features/todos',
+    },
+    {
+      name: 'GITHUB FINDER',
+      description: 'Application to find out github user. Render all respositories of this user',
+      to: '/features/gitusers',
+    },
+  ],
 });
